1520b1ef0af11a9286eb4646876c93aa4e07e8ab
Neelesh Sai <rneeleshsai@gmail.com>
2020-08-22 13:43:53 +0530

    updates

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,6 +4,6 @@ const router = express.Router();
 //@route  GET api/auth
 //@desc   Test route
 //@access Public
-router.get('/', (req, res) => res.send('Auth Route'));
+router.post('/', (req, res) => res.send('Auth Route'));
 
 module.exports = router;
